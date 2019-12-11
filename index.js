4ea89700d8eeada6ffc1349556e4c61bf7ccbb3e
Mark Louie Ferrer <kram123f@Marks-MacBook-Air.local>
2019-12-11 23:53:24 +0800

    simple node app

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const suser = require('supertest')
+const app = express()
+const port = 3000
+app.get('/',(req,res) => res.send('Hello'))
+
+ 
+app.listen(port, () => console.log(`Running at port ${port}`))
+ 
