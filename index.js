f30af2dac9eee18bb0e9adcf905825881f950df1
Mark Louie Ferrer <kram123f@Marks-MacBook-Air.local>
2019-12-12 00:14:09 +0800

    some update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const suser = require('supertest')
 const app = express()
 const port = 3000
-app.get('/',(req,res) => res.send('Hello'))
+app.get('/',(req,res) => res.send('Hello Future Airline'))
 
  
 app.listen(port, () => console.log(`Running at port ${port}`))
